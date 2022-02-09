881312fe6888abbc921b77f3817e6c9c1ebf6f62
ikkyy06 <krniawn.ikhsan@gmail.com>
2022-02-09 22:30:09 +0700

    Alhamdulillah tournament beres :)

---
+++ app/Http/Controllers/TournamentController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Tournament;
+
+class TournamentController extends Controller
+{
+    public function index()
+    {
+        $tournament = Tournament::get();
+
+        return response()->json($tournament, 200);
+    }
+
+    public function show($id)
+    {
+        $tournament = Tournament::find($id);
+
+        return response()->json($tournament, 200);
+    }
+
+    public function store()
+    {
+        $attr = request()->all();
+        $tournament = Tournament::create($attr);
+
+        return response()->json($tournament, 200);
+    }
+
+    public function update($id)
+    {
+        $tournament = Tournament::find($id);
+
+        $tournament->name = request()->input('name');
+        $tournament->save();
+
+        return response()->json($tournament, 200);
+    }
+
+    public function delete($id)
+    {
+        $tournament = Tournament::find($id);
+
+        $tournament->delete();
+        $message = ['message' => 'delete successfully', 'id_tournament' => $id];
+        return response()->json($message, 200);
+    }
+}
